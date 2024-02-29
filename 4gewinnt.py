84c49d2d667eccbbf56c701e4a237bab4f46d28e
maratin23 <157828439+maratin23@users.noreply.github.com>
2024-02-29 13:17:18 +0100

    Update 4gewinnt.py

--- 4gewinnt.py
+++ 4gewinnt.py
@@ -14,13 +14,13 @@ while a < 28:
     b = 8
     while b < 26: 
         offset_canvas.SetPixel(a, b, 255, 255, 255)  
-        offset_canvas = matrix.CreateFrameCanvas(offset_canvas)
+        offset_canvas = matrix.CreateFrameCanvas()
         b += 3
     
     a += 1
 c = 6
 while c < 28:
     offset_canvas.SetPixel(25, c, 255, 255, 255)
-    offset_canvas = matrix.CreateFrameCanvas(offset_canvas)
+    offset_canvas = matrix.CreateFrameCanvas()
     c += 1
 
