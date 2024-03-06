a494398132c98a754e205c278b3b5863d6bad1dc
DianaTin23 <157728765+DianaTin23@users.noreply.github.com>
2024-03-06 16:21:50 +0100

    Update test-gamepad.py

--- test-gamepad.py
+++ test-gamepad.py
@@ -1,88 +1,151 @@
-#!/usr/bin/env python
+import pygame
 
-import os, sys, pygame
-from pygame import locals
+pygame.init()
 
-# Define the colors we will use in RGB format
-black = [0, 0, 0]
-white = [255, 255, 255]
-red = [255, 0, 0]
 
-# Set the resolution for the 32x32 RGB matrix
-matrix_resolution = [32, 32]
+# This is a simple class that will help us print to the screen.
+# It has nothing to do with the joysticks, just outputting the
+# information.
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 25)
 
-# Adjust the screen size accordingly
-size = [matrix_resolution[0] * 20, matrix_resolution[1] * 10]
-screen = pygame.display.set_mode(size)
+    def tprint(self, screen, text):
+        text_bitmap = self.font.render(text, True, (0, 0, 0))
+        screen.blit(text_bitmap, (self.x, self.y))
+        self.y += self.line_height
 
-# Fill the screen with white
-screen.fill(white)
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
 
-# Put something in the application Bar
-pygame.display.set_caption("Testing gamepad")
+    def indent(self):
+        self.x += 10
 
-pygame.draw.rect(screen, black, (1, 1, matrix_resolution[0] * 2 - 2, matrix_resolution[1] * 2 - 2), 0)
+    def unindent(self):
+        self.x -= 10
 
-pygame.display.flip()
 
-pygame.joystick.init()  # main joystick device system
+def main():
+    # Set the width and height of the screen (width, height), and name the window.
+    screen = pygame.display.set_mode((500, 700))
+    pygame.display.set_caption("Joystick example")
 
-# Initialize the font
-try:
-    textfont = pygame.font.SysFont("moonspace", 12)  # Change the font size to a reasonable value
-except pygame.error:
-    print('Font not initialized. Make sure the specified font is available.')
+    # Used to manage how fast the screen updates.
+    clock = pygame.time.Clock()
 
-done = True
+    # Get ready to print.
+    text_print = TextPrint()
 
-try:
-    j = pygame.joystick.Joystick(0)  # create a joystick instance
-    j.init()  # init instance
-    print('Enabled joystick: ' + j.get_name())
-    joyName = j.get_name()
-except pygame.error:
-    print('No joystick found.')
+    # This dict can be left as-is, since pygame will generate a
+    # pygame.JOYDEVICEADDED event for every joystick connected
+    # at the start of the program.
+    joysticks = {}
 
-pygame.draw.rect(screen, black, (1, 1, matrix_resolution[0] * 2 - 2, matrix_resolution[1] * 2 - 2), 0)
-pygame.draw.rect(screen, white, (matrix_resolution[0], matrix_resolution[1], 4, 4), 0)
-joyText = textfont.render("Gamepad : " + joyName, 1, red)
-screen.blit(joyText, (matrix_resolution[0] * 6, matrix_resolution[1] * 2))
+    done = False
+    while not done:
+        # Event processing step.
+        # Possible joystick events: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
+        # JOYBUTTONUP, JOYHATMOTION, JOYDEVICEADDED, JOYDEVICEREMOVED
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True  # Flag that we are done so we exit this loop.
 
-pygame.display.flip()
+            if event.type == pygame.JOYBUTTONDOWN:
+                print("Joystick button pressed.")
+                if event.button == 0:
+                    joystick = joysticks[event.instance_id]
+                    if joystick.rumble(0, 0.7, 500):
+                        print(f"Rumble effect played on joystick {event.instance_id}")
 
-while done:
-    for e in pygame.event.get():  # iterate over event stack
-        if e.type == pygame.QUIT:
-            done = False
+            if event.type == pygame.JOYBUTTONUP:
+                print("Joystick button released.")
 
-        if e.type == locals.JOYAXISMOTION:  # Read Analog Joystick Axis
-            x1, y1 = j.get_axis(0), j.get_axis(1)  # Left Stick
+            # Handle hotplugging
+            if event.type == pygame.JOYDEVICEADDED:
+                # This event will be generated when the program starts for every
+                # joystick, filling up the list without needing to create them manually.
+                joy = pygame.joystick.Joystick(event.device_index)
+                joysticks[joy.get_instance_id()] = joy
+                print(f"Joystick {joy.get_instance_id()} connencted")
 
-            print(x1)
-            print(y1)
-            x1Text = textfont.render("x : " + str(x1), 1, red)
-            y1Text = textfont.render("y : " + str(y1), 1, red)
-            pygame.draw.rect(screen, black, (1, 1, matrix_resolution[0] * 2 - 2, matrix_resolution[1] * 2 - 2), 0)
-            pygame.draw.rect(screen, white, (matrix_resolution[0] + int(x1 * 16), matrix_resolution[1] + int(y1 * 16), 4, 4), 0)
-            pygame.draw.rect(screen, white,
-                             (matrix_resolution[0] * 2, matrix_resolution[1] * 3, matrix_resolution[0] * 2,
-                              matrix_resolution[1] * 2), 0)
-            screen.blit(x1Text, (matrix_resolution[0] * 6, matrix_resolution[1] * 3))
-            screen.blit(y1Text, (matrix_resolution[0] * 6, matrix_resolution[1] * 4))
-            pygame.display.flip()
+            if event.type == pygame.JOYDEVICEREMOVED:
+                del joysticks[event.instance_id]
+                print(f"Joystick {event.instance_id} disconnected")
 
-        if e.type == locals.JOYBUTTONDOWN:  # Read the buttons
-            print("button down" + str(e.button))
-            for i in range(0, 10):
-                if e.button == i:
-                    pygame.draw.rect(screen, red, (matrix_resolution[0] * 2 + i * 2, matrix_resolution[1] * 2, 2, 2), 0)
-                    buttonText = textfont.render(str(i), 1, white)
-                    screen.blit(buttonText, (matrix_resolution[0] * 2 + i * 2 + 1, matrix_resolution[1] * 2))
-                    pygame.display.flip()
+        # Drawing step
+        # First, clear the screen to white. Don't put other drawing commands
+        # above this, or they will be erased with this command.
+        screen.fill((255, 255, 255))
+        text_print.reset()
 
-        if e.type == locals.JOYBUTTONUP:  # Read the buttons
-            print("button up" + str(e.button))
-            for i in range(0, 10):
-                if e.button == i:
-                    pygame.draw.rect(screen, white, (matrix_resolution[0] * 2 + i * 2, matrix_resolution[1] * 2, 2, 2), 0)
-                    pygame.display.flip()
+        # Get count of joysticks.
+        joystick_count = pygame.joystick.get_count()
+
+        text_print.tprint(screen, f"Number of joysticks: {joystick_count}")
+        text_print.indent()
+
+        # For each joystick:
+        for joystick in joysticks.values():
+            jid = joystick.get_instance_id()
+
+            text_print.tprint(screen, f"Joystick {jid}")
+            text_print.indent()
+
+            # Get the name from the OS for the controller/joystick.
+            name = joystick.get_name()
+            text_print.tprint(screen, f"Joystick name: {name}")
+
+            guid = joystick.get_guid()
+            text_print.tprint(screen, f"GUID: {guid}")
+
+            power_level = joystick.get_power_level()
+            text_print.tprint(screen, f"Joystick's power level: {power_level}")
+
+            # Usually axis run in pairs, up/down for one, and left/right for
+            # the other. Triggers count as axes.
+            axes = joystick.get_numaxes()
+            text_print.tprint(screen, f"Number of axes: {axes}")
+            text_print.indent()
+
+            for i in range(axes):
+                axis = joystick.get_axis(i)
+                text_print.tprint(screen, f"Axis {i} value: {axis:>6.3f}")
+            text_print.unindent()
+
+            buttons = joystick.get_numbuttons()
+            text_print.tprint(screen, f"Number of buttons: {buttons}")
+            text_print.indent()
+
+            for i in range(buttons):
+                button = joystick.get_button(i)
+                text_print.tprint(screen, f"Button {i:>2} value: {button}")
+            text_print.unindent()
+
+            hats = joystick.get_numhats()
+            text_print.tprint(screen, f"Number of hats: {hats}")
+            text_print.indent()
+
+            # Hat position. All or nothing for direction, not a float like
+            # get_axis(). Position is a tuple of int values (x, y).
+            for i in range(hats):
+                hat = joystick.get_hat(i)
+                text_print.tprint(screen, f"Hat {i} value: {str(hat)}")
+            text_print.unindent()
+
+            text_print.unindent()
+
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+        # Limit to 30 frames per second.
+        clock.tick(30)
+
+
+if __name__ == "__main__":
+    main()
+    # If you forget this line, the program will 'hang'
+    # on exit if running from IDLE.
+    pygame.quit()
