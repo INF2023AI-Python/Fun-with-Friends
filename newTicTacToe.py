84c70dd11ffd01ddaeaf45a06369849aa4787219
DianaTin23 <157728765+DianaTin23@users.noreply.github.com>
2024-03-07 00:33:03 +0100

    Update newTicTacToe.py

--- newTicTacToe.py
+++ newTicTacToe.py
@@ -1,6 +1,6 @@
 import time
-from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics#from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
-#from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions, graphics
+#from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions, graphics
 
 # Konfiguration der LED-Matrix
 options = RGBMatrixOptions()
@@ -14,20 +14,20 @@ matrix = RGBMatrix(options=options)
 # Funktion zum Zeichnen des Tictactoe-Boards auf der RGB-LED-Matrix
 def draw_board(board_state):
     matrix.Clear()
-    for row in range(31):
-        for col in range(31):
+    for row in range(32):
+        for col in range(32):
             # Zeichne das Raster
-            if row % 11 == 0 or col % 11 == 0:
+            if row % 10 == 0 or col % 10 == 0:
                 matrix.SetPixel(col, row, 100, 100, 100)
 
     # Zeichne die Spielsymbole
     for row in range(3):
         for col in range(3):
             if board_state[row][col] == 'O':
-                graphics.DrawCircle(matrix, col * 11 + 5, row * 11 + 5, 4, graphics.Color(0, 0, 255))
+                graphics.DrawCircle(matrix, col * 10 + 5, row * 10 + 5, 4, graphics.Color(0, 0, 255))
             elif board_state[row][col] == 'X':
-                graphics.DrawLine(matrix, col * 11 + 1, row * 11 + 1, col * 11 + 9, row * 11 + 9, graphics.Color(255, 0, 0))
-                graphics.DrawLine(matrix, col * 11 + 9, row * 11 + 1, col * 11 + 1, row * 11 + 9, graphics.Color(255, 0, 0))
+                graphics.DrawLine(matrix, col * 10 + 1, row * 10 + 1, col * 10 + 9, row * 10 + 9, graphics.Color(255, 0, 0))
+                graphics.DrawLine(matrix, col * 10 + 9, row * 10 + 1, col * 10 + 1, row * 10 + 9, graphics.Color(255, 0, 0))
 
 # Funktion zum Überprüfen des Spielstatus (Gewonnen, Unentschieden usw.)
 def check_winner(board_state):
