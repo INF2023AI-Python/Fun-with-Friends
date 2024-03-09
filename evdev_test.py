ae12086bbb36ef1d09e17ed24446cc1bab4d2935
maratin23 <157828439+maratin23@users.noreply.github.com>
2024-03-09 10:09:42 +0100

    Update evdev_test.py

--- evdev_test.py
+++ evdev_test.py
@@ -1,4 +1,8 @@
-from evdev import InputDevice, categorize, ecodes
+import sys
+
+sys.path.append("/home/pi/.local/lib/python3.9/site-packages")
+
+import evdev
 
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 
