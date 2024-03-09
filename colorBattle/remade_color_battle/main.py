2b55182d845662bf4e6e86d0fc4c3345342dc85b
Zoehong11 <148884098+Zoehong11@users.noreply.github.com>
2024-03-09 10:06:38 +0100

    Update main.py

--- colorBattle/remade_color_battle/main.py
+++ colorBattle/remade_color_battle/main.py
@@ -8,7 +8,10 @@ from colorBattle.remade_color_battle.movement import wrapping, input
 # Constants and Configurations
 SCREEN_WIDTH = 32
 SCREEN_HEIGHT = 32
+PLAY_WIDTH = 32
+PLAY_HEIGHT = 28
 GAME_DURATION = 60
+
 
 global player1_x, player1_y, player2_x, player2_y, game_area, player_size
 player1_speed = 10
@@ -16,6 +19,7 @@ player2_speed = 10
 player_size = 1
 game_area = [[0 for _ in range(SCREEN_WIDTH)] for _ in range(SCREEN_HEIGHT)]
 
+#initialise
 pygame.init
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
