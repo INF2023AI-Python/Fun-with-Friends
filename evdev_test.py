c7eb1b44c599c7d15fd46eefabad7318fae642db
maratin23 <157828439+maratin23@users.noreply.github.com>
2024-03-09 10:31:01 +0100

    Update evdev_test.py

--- evdev_test.py
+++ evdev_test.py
@@ -3,19 +3,41 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 sys.path.append("/home/pi/.local/lib/python3.9/site-packages")
 
-from evdev import InputDevice, categorize, ecodes
+#from evdev import InputDevice, categorize, ecodes
 
-dev = InputDevice('/dev/input/event4')
+#dev = InputDevice('/dev/input/event4')
 
-print(dev)
+#print(dev)
 
-for event in dev.read_loop():
+#for event in dev.read_loop():
 
-    if event.type == ecodes.EV_KEY:
+   # if event.type == ecodes.EV_KEY:
 
-        print(categorize(event))
+      #  print(categorize(event))
 
-dev.active_keys(verbose=True)
 
-dev.active_keys()
+import evdev
 
+# Finde den Pfad des Controllers
+controller_path = None
+for device in [evdev.InputDevice(path) for path in evdev.list_devices()]:
+    if "gamepad" in device.name.lower():
+        controller_path = device.path
+        break
+
+if controller_path is None:
+    print("Kein Controller gefunden.")
+else:
+    print("Controller gefunden:", controller_path)
+
+# Öffne den Controller
+controller = evdev.InputDevice(controller_path)
+
+# Funktion zum Behandeln von Tastendrücken
+def handle_key_event(event):
+    if event.type == evdev.ecodes.EV_KEY:
+        print("Taste gedrückt:", event.code)
+
+# Hauptprogramm
+for event in controller.read_loop():
+    handle_key_event(event)
