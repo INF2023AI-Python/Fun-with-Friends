c2f9d52a8d0b3674e3613a87774438169d7f3a21
DianaTin23 <157728765+DianaTin23@users.noreply.github.com>
2024-03-13 20:22:21 +0100

    Update startbildschirm.py

--- startbildschirm.py
+++ startbildschirm.py
@@ -153,6 +153,8 @@ def select_option(new_position):
     elif new_position[0] == 1 and new_position[1] == 0:       
         run_game()
         print("Tictactoe wurde ausgewählt")
+        # Nach dem Spiel zu Spielauswahl zurückkehren
+        main()
     elif new_position[0] == 0 and new_position[1] == 1:
         print("VierGewinnt wurde ausgewählt")
     elif new_position[0] == 1 and new_position[1] == 1:
@@ -208,7 +210,7 @@ def main():
         pygame.quit()
         quit()
 
-    # Wähle den ersten verfügbaren Joystick was
+    # Wähle den ersten verfügbaren Joystick
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
 
