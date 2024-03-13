569fd14a3fed934ea2415ebd1d3612d40ba21076
DianaTin23 <157728765+DianaTin23@users.noreply.github.com>
2024-03-13 16:14:05 +0100

    Update testexefile.py

--- testexefile.py
+++ testexefile.py
@@ -18,14 +18,5 @@ def draw_board():
             if row % 10 == 0 or col % 10 == 0:
                 matrix.SetPixel(col, row, 100, 100, 100)
 
-    # Zeichne die Spielsymbole
-    for row in range(3):
-        for col in range(3):
-            if board_state[row][col] == 'O':
-                graphics.DrawCircle(matrix, col * 10 + 5, row * 10 + 5, 4, graphics.Color(0, 0, 255))
-            elif board_state[row][col] == 'X':
-                graphics.DrawLine(matrix, col * 10 + 1, row * 10 + 1, col * 10 + 9, row * 10 + 9, graphics.Color(255, 0, 0))
-                graphics.DrawLine(matrix, col * 10 + 9, row * 10 + 1, col * 10 + 1, row * 10 + 9, graphics.Color(255, 0, 0))
-
 while True:
         draw_board()
