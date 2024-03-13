a1a3b731ddc858eb044064e0f2516d3326049e4f
maratin23 <157828439+maratin23@users.noreply.github.com>
2024-03-13 10:55:53 +0100

    Update VierGewinnt.py

--- VierGewinnt.py
+++ VierGewinnt.py
@@ -42,14 +42,14 @@ def display_board():
 def check_win(player):
     # Horizontale Linie
     for r in range(ROWS):
-        for c in range(COLS - 3): 
-            if all(board[r][c + i] == player for i in range(3)):
+        for c in range(COLS - 4): 
+            if all(board[r][c + i] == player for i in range(4)):
                 return True
             
     # Vertikale Linie
     for c in range(COLS):
-        for r in range(ROWS - 4):
-            if all(board[r + i][c] == player for i in range(4)):
+        for r in range(ROWS - 3):
+            if all(board[r + i][c] == player for i in range(3)):
                 return True
         
     # Diagonal nach oben rechts
