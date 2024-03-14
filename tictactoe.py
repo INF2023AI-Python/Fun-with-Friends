a2538bd1b73288cf0bf34ba5ed3a4596c31dcfac
DianaTin23 <157728765+DianaTin23@users.noreply.github.com>
2024-03-14 20:39:29 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -98,14 +98,8 @@ def set_x_or_o(board_state):
     # Überprüfe, ob das ausgewählte Feld leer ist (' ')
     if board_state[orange_square_position[1]][orange_square_position[0]] == ' ':
         board_state[orange_square_position[1]][orange_square_position[0]] = current_player
-        if check_winner(board_state):
-            current_player = 'X' if current_player == 'O' else 'O'  # Wechsle den aktuellen Spieler
-            print(f"Spieler {current_player} gewinnt!")
-            return True  # Rückgabe, um den Gewinnerbildschirm zuletzt anzuzeigen
-        else:
-            current_player = 'X' if current_player == 'O' else 'O'  # Wechsle den aktuellen Spieler
-            print(f"Spieler {current_player}")
-    return False
+        current_player = 'X' if current_player == 'O' else 'O'  # Wechsle den aktuellen Spieler
+        print(f"Player {current_player}")
 
 # Funktion zum Erstellen eines Canvas und Anzeigen des Texts
 def display_text(text, color):
@@ -125,11 +119,14 @@ def display_text(text, color):
 
 # Darstellung Gewinnbildschirm
 def display_winner(player):
+    global current_player
     if player == 'X':
+        current_player = 'O'  # Wechsle den aktuellen Spieler
         color = (255, 0, 0)  # Rot für Spieler X
         display_text(["WIN", "Player", "X"], color)
         time.sleep(5)
     elif player == 'O':
+        current_player = 'X'  # Wechsle den aktuellen Spieler
         color = (0, 0, 255)  # Blau für Spieler O
         display_text(["WIN", "Player", "O"], color)
         time.sleep(5)
@@ -167,13 +164,13 @@ def tictactoe():
 
         # Überprüfen Sie den Gewinner und den Unentschieden-Status
         if check_winner(board_state):
+            print(f"Player {current_player} wins!")
             draw_board(board_state)  # Aktualisiere das letzte Mal vor dem Ende, um den Gewinner anzuzeigen
-            print(f"Spieler {current_player} gewinnt!")
             display_winner(current_player)  # Zeige Gewinnmeldung auf der LED-Matrix an
             return
         elif ' ' not in [cell for row in board_state for cell in row]:
+            print("It's a draw!")
             draw_board(board_state)  # Aktualisiere das letzte Mal vor dem Ende, um das Unentschieden anzuzeigen
-            print("Unentschieden!")
             display_draw()  # Zeige Unentschiedenmeldung auf der LED-Matrix an
             return
 
