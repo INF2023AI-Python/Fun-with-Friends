e63073a7913a3340c0b24ee21766159dcdfee69f
Danylo Tulainov <tulainovd.tin23@student.dhbw-heidenheim.de>
2024-03-15 14:41:38 +0100

    update game_loop.py

--- colorBattle/remade_color_battle/emulator.py
+++ colorBattle/remade_color_battle/emulator.py
@@ -1,20 +1,53 @@
-import RGBMatrixEmulator
+# # import RGBMatrixEmulator
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+#
+#
+# ROWS = 32
+# COLS = 32
+#
+#
+# class Emulator:
+#     def __init__(self, rows, cols, hardware_mapping='adafruit-hat-pwm'):
+#         # Initialize RGBMatrix options
+#         options = RGBMatrixOptions()
+#         options.hardware_mapping = hardware_mapping
+#         options.rows = rows
+#         options.cols = cols
+#
+#         # Create RGBMatrix and canvas
+#         self.matrix = RGBMatrix.RGBMatrix(options)
+#         self.canvas = self.matrix.CreateFrameCanvas()
+#
+#     def update_canvas(self, grid):
+#         # Clear the canvas
+#         self.canvas.Clear()
+#
+#         # Draw the game state
+#         for i in range(ROWS):
+#             for j in range(COLS):
+#                 color = grid[i][j]
+#                 self.canvas.SetPixel(i, j, *color)
+#
+#         # Update the display
+#         self.matrix.SwapOnVSync(self.canvas)
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
 
 ROWS = 32
-COLS = 32
+COLS = 28
 
 
 class Emulator:
     def __init__(self, rows, cols, hardware_mapping='adafruit-hat-pwm'):
         # Initialize RGBMatrix options
-        options = RGBMatrixEmulator.RGBMatrixOptions()
+        options = RGBMatrixOptions()
         options.hardware_mapping = hardware_mapping
         options.rows = rows
         options.cols = cols
 
         # Create RGBMatrix and canvas
-        self.matrix = RGBMatrixEmulator.RGBMatrix(options)
+        self.matrix = RGBMatrix(options=options)
         self.canvas = self.matrix.CreateFrameCanvas()
 
     def update_canvas(self, grid):
