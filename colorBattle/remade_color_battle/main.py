b629194093dcbb23e863ce4f41ec700b5520c703
Zoehong11 <148884098+Zoehong11@users.noreply.github.com>
2024-03-16 12:49:47 +0100

    Update main.py

--- colorBattle/remade_color_battle/main.py
+++ colorBattle/remade_color_battle/main.py
@@ -49,7 +49,7 @@ def main():
         # Draw the updated scoreboard
         scoreboard.draw()
 
-        clock.tick(120)
+        clock.tick(200)
         # Update the display
         matrix.SwapOnVSync(offset_canvas)
 
