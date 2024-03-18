eb34362037c0f785ec1ada1c5666abe3d7b3a614
Danylo Tulainov <tulainovd.tin23@student.dhbw-heidenheim.de>
2024-03-18 18:56:54 +0100

    update controllers.py

--- colorBattle/remade_color_battle/controllers.py
+++ colorBattle/remade_color_battle/controllers.py
@@ -4,22 +4,22 @@ def controllers(joysticks, player1, player2, maze_pattern, game_area):
         axis_y = joystick.get_axis(1)
 
         if i == 0:  # Player 1 controls (First gamepad)
-            if axis_x < -1:
+            if axis_x < -0.7:
                 player1.move('LEFT', maze_pattern, game_area)
-            elif axis_x > 1:
+            elif axis_x > 0.7:
                 player1.move('RIGHT', maze_pattern, game_area)
-            if axis_y < -1:
+            if axis_y < -0.7:
                 player1.move('UP', maze_pattern, game_area)
-            elif axis_y > 1:
+            elif axis_y > 0.7:
                 player1.move('DOWN', maze_pattern, game_area)
 
         elif i == 1:  # Player 2 controls (Second gamepad)
-            if axis_x < -1:
+            if axis_x < -0.7:
                 player2.move('LEFT', maze_pattern, game_area)
-            elif axis_x > 1:
+            elif axis_x > 0.7:
                 player2.move('RIGHT', maze_pattern, game_area)
-            if axis_y < -1:
+            if axis_y < -0.7:
                 player2.move('UP', maze_pattern, game_area)
-            elif axis_y > 1:
+            elif axis_y > 0.7:
                 player2.move('DOWN', maze_pattern, game_area)
 
