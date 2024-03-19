d9eac593c4fd42e54822eed88eeba5fe0e1627f1
Zoehong11 <148884098+Zoehong11@users.noreply.github.com>
2024-03-19 19:29:05 +0100

    Update scoreboard.py

--- colorBattle/remade_color_battle/scoreboard.py
+++ colorBattle/remade_color_battle/scoreboard.py
@@ -1,6 +1,7 @@
 # from game_loop import player1_points, player2_points
 from rgbmatrix import graphics
 import pygame
+import time
 
 # from RGBMatrixEmulator import graphics
 SCREEN_WIDTH = 32
@@ -9,9 +10,19 @@ SCREEN_HEIGHT = 32
 class Scoreboard:
     def __init__(self, canvas):
         self.canvas = canvas
+
         self.player1_points = 0
         self.player2_points = 0
-        self.start_time = pygame.time.get_ticks()
+
+        self.start_time = time.time()
+
+        blau = (0,255,255)
+        turquoise = (0,0,255)
+        self.pointsColor = graphics.Color(*blau)
+        self.timeColor = graphics.Color(*turquoise)
+
+        self.font = graphics.Font()
+        self.font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/4x6.bdf")
 
     def update(self, duration):
         # self.player1_points = player1_points
@@ -19,26 +30,21 @@ class Scoreboard:
         # self.remaining_time = duration
         
         # Calculate the elapsed time since the start of the game
-        elapsed_time_ms = pygame.time.get_ticks() - self.start_time
-        elapsed_time_seconds = elapsed_time_ms // 1000  # Convert milliseconds to seconds
-        self.remaining_time = max(60 - elapsed_time_seconds, 0)  # Calculate remaining time
+        elapsed_time = time.time - self.start_time
+        remaining_time = duration - elapsed_time
+        return remaining_time
+    
+    def draw(self, offset_canvas, duration):
 
-    def draw(self, offset_canvas):
-
-        font = graphics.Font()
-        font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/4x6.bdf")
-        blau = (0,255,255)
-        turquoise = (0,0,255)
-        pointsColor = graphics.Color(*blau)
-        timeColor = graphics.Color(*turquoise)
+        remaining_seconds = Scoreboard.update(self, duration)
 
        # Draw player 1's points on the left side
-        graphics.DrawText(offset_canvas, font, 1, SCREEN_HEIGHT, pointsColor, str(self.player1_points))
+        graphics.DrawText(offset_canvas, self.font, 1, SCREEN_HEIGHT, self.pointsColor, str(self.player1_points))
 
         # Draw player 2's points on the right side
-        graphics.DrawText(offset_canvas, font, SCREEN_WIDTH - 4, SCREEN_HEIGHT, pointsColor, str(self.player2_points))
+        graphics.DrawText(offset_canvas, self.font, SCREEN_WIDTH - 4, SCREEN_HEIGHT, self.pointsColor, str(self.player2_points))
 
         # Draw remaining time in the middle
-        remaining_seconds = self.remaining_time % 60
+        # remaining_seconds = self.remaining_time % 60
         time_text = f"{remaining_seconds:02d}"
-        graphics.DrawText(offset_canvas, font, SCREEN_WIDTH // 2 - len(time_text) -2 // 2, SCREEN_HEIGHT, timeColor, time_text)
+        graphics.DrawText(offset_canvas, font, SCREEN_WIDTH // 2 - len(time_text) -2 // 2, SCREEN_HEIGHT, self.timeColor, time_text)
