f51a3de5bc6130ef71a2846002e5ab50e9f3a7c2
Zoehong11 <148884098+Zoehong11@users.noreply.github.com>
2024-03-21 17:23:07 +0100

    Update scoreboard.py

--- colorBattle/remade_color_battle/scoreboard.py
+++ colorBattle/remade_color_battle/scoreboard.py
@@ -5,10 +5,10 @@ SCREEN_WIDTH = 32
 SCREEN_HEIGHT = 32
 
 class Scoreboard:
-    def __init__(self, canvas, player1, player2):
+    def __init__(self, canvas):
         self.canvas = canvas
-        self.player1 = player1
-        self.player2 = player2
+        self.player1_points = 0
+        self.player2_points = 0
         self.start_time = time.time()
 
         blau = (0, 255, 255)
@@ -31,7 +31,7 @@ class Scoreboard:
         return remaining_seconds
 
     def clear_time_text_area(self, offset_canvas):
-        # Clear the area occupied by the previous time text using black color
+    # Clear the area occupied by the previous time text using black color
         for x in range(32):
             for y in range(26, 32):
                 offset_canvas.SetPixel(x, y, 0, 0, 0)  # Set pixel to black
@@ -45,16 +45,21 @@ class Scoreboard:
         print(f"score2 is {self.player2_points}")
         remaining_seconds = self.update(duration)
         print("Remaining Time:", remaining_seconds)  # Check the remaining time in the console
+
         # Clear the area occupied by the previous time text
         self.clear_time_text_area(offset_canvas)
+
         # Draw player 1's points on the left side
         graphics.DrawText(offset_canvas, self.font, 1, SCREEN_HEIGHT, self.pointsColor, str(self.player1_points))
+
         # Draw player 2's points on the right side
         graphics.DrawText(offset_canvas, self.font, SCREEN_WIDTH - 4, SCREEN_HEIGHT, self.pointsColor, str(self.player2_points))
+
         # Draw remaining time in the middle
         time_text = f"{int(remaining_seconds):02d}"
         self.time_text_width = len(time_text) * 3  # Calculate the width of the time text
         self.time_text_x = SCREEN_WIDTH // 2 - self.time_text_width // 2  # Calculate the x-coordinate of the time text
         self.time_text_y = SCREEN_HEIGHT  # Set the y-coordinate of the time text
         graphics.DrawText(offset_canvas, self.font, self.time_text_x, self.time_text_y, self.timeColor, time_text)
+
         return remaining_seconds
