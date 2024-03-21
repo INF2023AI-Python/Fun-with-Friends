11792f8f6ab4c8c088b19e463089d39b5605b508
Danylo Tulainov <tulainovd.tin23@student.dhbw-heidenheim.de>
2024-03-21 12:02:57 +0100

    shit doesn't work

--- colorBattle/remade_color_battle/controllers.py
+++ colorBattle/remade_color_battle/controllers.py
@@ -1,34 +1,24 @@
-
-
 def controllers(joysticks, player1, player2, maze_pattern, game_area):
-    DEAD_ZONE = 0.07  # Adjust this value to increase or decrease the dead zone
     for i, joystick in enumerate(joysticks):
-        axis_x = joystick.get_axis(1)  # Swap the axes
-        axis_y = joystick.get_axis(0)  # Swap the axes
-
-        if abs(axis_x) < DEAD_ZONE:
-            axis_x = 0
-        if abs(axis_y) < DEAD_ZONE:
-            axis_y = 0
+        axis_x = joystick.get_axis(0)
+        axis_y = joystick.get_axis(1)
 
         if i == 0:  # Player 1 controls (First gamepad)
-            if axis_x < -DEAD_ZONE:
+            if -0.2 < axis_x < 0.2 and axis_y < -0.8:
                 player1.move('UP', maze_pattern, game_area)
-            elif axis_x > DEAD_ZONE:
+            elif -0.2 < axis_x < 0.2 and axis_y > 0.8:
                 player1.move('DOWN', maze_pattern, game_area)
-            if axis_y < -DEAD_ZONE:
-                player1.move('LEFT', maze_pattern, game_area)
-            elif axis_y > DEAD_ZONE:
+            elif axis_x > 0.8 and -0.2 < axis_y < 0.2:
                 player1.move('RIGHT', maze_pattern, game_area)
+            elif axis_x < -0.8 and -0.2 < axis_y < 0.2:
+                player1.move('LEFT', maze_pattern, game_area)
 
         elif i == 1:  # Player 2 controls (Second gamepad)
-            if axis_x < -DEAD_ZONE:
+            if -0.2 < axis_x < 0.2 and axis_y < -0.8:
                 player2.move('UP', maze_pattern, game_area)
-            elif axis_x > DEAD_ZONE:
+            elif -0.2 < axis_x < 0.2 and axis_y > 0.8:
                 player2.move('DOWN', maze_pattern, game_area)
-            if axis_y < -DEAD_ZONE:
-                player2.move('LEFT', maze_pattern, game_area)
-            elif axis_y > DEAD_ZONE:
+            elif axis_x > 0.8 and -0.2 < axis_y < 0.2:
                 player2.move('RIGHT', maze_pattern, game_area)
-
-        # print(f"Joystick {i}: axis_x = {axis_x}, axis_y = {axis_y}")
+            elif axis_x < -0.8 and -0.2 < axis_y < 0.2:
+                player2.move('LEFT', maze_pattern, game_area)
