13094d02ed752070b675905d79124c4857f18666
Zoehong11 <148884098+Zoehong11@users.noreply.github.com>
2024-03-21 16:48:32 +0100

    Update main.py

--- colorBattle/remade_color_battle/main.py
+++ colorBattle/remade_color_battle/main.py
@@ -1,24 +1,20 @@
-# from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 import pygame
-from obstacle import obstacle, maze
-from scoreboard import Scoreboard
-from levelSelection import select_level
+import os
 from player import Player
 from controllers import controllers
-
-# from game_loop import player1_points, player2_points
-# pip install numpy
+from obstacle import obstacle
+from levelSelection import select_level
+from scoreboard import Scoreboard
 
 # Constants and Configurations
 SCREEN_WIDTH = 32
 SCREEN_HEIGHT = 32
 PLAY_WIDTH = 32
 PLAY_HEIGHT = 26
-
-#set duration
 GAME_DURATION = 60
 
+# Initialize RGBMatrix
 options = RGBMatrixOptions()
 options.rows = 32
 options.chain_length = 1
@@ -28,8 +24,9 @@ options.drop_privileges = 0
 matrix = RGBMatrix(options=options)
 offset_canvas = matrix.CreateFrameCanvas()
 
-# Initialise
+# Initialise Pygame
 pygame.init()
+os.environ["XDG_RUNTIME_DIR"] = "/path/to/runtime/directory"
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 
@@ -38,19 +35,20 @@ grid = [[(0, 0, 0) for _ in range(PLAY_HEIGHT)] for _ in range(PLAY_WIDTH)]
 
 pygame.display.set_caption("Color Battle")
 
-# initialize players
+# Initialize players
 player1 = Player((255, 255, 0), (255, 0, 0), (PLAY_HEIGHT // 2, PLAY_WIDTH // 2 - 10))
 player2 = Player((0, 0, 255), (0, 255, 0), (PLAY_HEIGHT // 2, PLAY_WIDTH // 2 + 10))
-# create an instance for scoreboard
-scoreboard = Scoreboard(offset_canvas, player1, player2)
+
+# Create an instance for scoreboard
+scoreboard = Scoreboard(offset_canvas)
 
 # Easy mode
 game_area = obstacle(offset_canvas, matrix)
+
 # Hard mode: maze
 maze_pattern = maze(offset_canvas, matrix)
 
 remaining_seconds = GAME_DURATION
-
 
 def main():
     running = True
@@ -58,9 +56,7 @@ def main():
 
     # select the level, easy or hard
     select_level(matrix, offset_canvas, joysticks)
-    # obstacle(offset_canvas, matrix)
-    # maze(offset_canvas, matrix)
-
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -68,16 +64,15 @@ def main():
 
         # Player controls
         controllers(joysticks, player1, player2, maze_pattern, game_area)
+        
         # Painting
         player1.paint(offset_canvas)
         player2.paint(offset_canvas)
 
+        # Count points and determine the winner
         player1.update_state(grid)
         player2.update_state(grid)
-
-        # Count points and determine the winner
-        # player1_points, player2_points = count_points(grid, player1.color, player2.color)
-        # print(f"{player1_points} and {player2_points}")
+        
         if player1.cells_painted == player2.cells_painted:
             print("It's a tie!")
         elif player1.cells_painted > player2.cells_painted:
@@ -85,7 +80,7 @@ def main():
         else:
             print("Player 2 wins!")
 
-        # Draw the updated scoreboard, NEED TO MAKE SURE DRAW ON THE SAME CANVAS
+        # Draw the updated scoreboard
         remaining_seconds = scoreboard.draw(offset_canvas, GAME_DURATION, player1, player2)
 
         # Check if remaining time is zero
@@ -95,12 +90,9 @@ def main():
         clock.tick(300)
         # Update the display
         matrix.SwapOnVSync(offset_canvas)
-        # Delay to control frame rate
-        # pygame.time.delay(1000)  # Delay for 1 second (1000 milliseconds)
 
     # Game over, quit Pygame
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
