798ed0a5a56812df5826af22b508f6a57f5c5683
DianaTin23 <157728765+DianaTin23@users.noreply.github.com>
2024-03-22 13:41:33 +0100

    update snake

--- snake/snake.py
+++ snake/snake.py
@@ -134,7 +134,7 @@ class Game:
         if not self.snake.move():
             print("The snake ate itself!")
             self.game_over()
-           # return
+            return
 
         head_position = self.snake.get_head_position()
 
@@ -148,7 +148,7 @@ class Game:
         if time.time() - self.start_time > 60:
             print("Time's up!")
             self.game_over()
-           # return
+            return
 
     def game_over(self):
         # Display "Game Over" on the matrix and stop the game
@@ -190,6 +190,8 @@ class Game:
                 print("Time's up!")
                 return
             self.clock.tick(10)
+            if self.game_over(self):
+                return
 
 
 class Fruit:
